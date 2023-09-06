9b82194e04843856ee034fc312cd170104d209d7
fahimkuet09 <de.fahimmf@gmail.com>
2023-09-06 21:29:39 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+const onDetect = (detectedCodes) => {
+  console.log(detectedCodes)
+  alert('Student Id :'+ detectedCodes[0].rawValue)
+}
+</script>
+
+<template>
+  <QrcodeStream @detect="onDetect" />
+
+</template>
+
+<style scoped></style>
